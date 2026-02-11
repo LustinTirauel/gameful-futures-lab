a57aaee4d8a87a461434bd9edc8a71e6749ff644
LustinTirauel <oguzturanburuk@gmail.com>
2026-02-11 21:54:06 +0200

    Add router-backed site layout with persistent LabScene

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { SiteLayout } from './layout/SiteLayout';
+import { HomePage } from './pages/HomePage';
+import { ProjectsPage } from './pages/ProjectsPage';
+import { PeoplePage } from './pages/PeoplePage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/people" element={<PeoplePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
